fa01267c8c6f8c64695a633a466a5f984bbb1d77
vagrant <vagrant@localhost.localdomain>
2022-11-20 01:41:02 +0000

    Adds custom OperatorFilterer and OwnedRegistrant

---
+++ src/custom/CustomOwnedRegistrant.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {IOperatorFilterRegistry} from "./../IOperatorFilterRegistry.sol";
+import {Ownable2Step} from "openzeppelin-contracts/access/Ownable2Step.sol";
+
+/**
+ * @title  CustomOwnedRegistrant
+ * @notice Ownable contract that registers itself with the custom OperatorFilterRegistry and administers its own entries,
+ *         to facilitate a subscription whose ownership can be transferred.
+ */
+contract CustomOwnedRegistrant is Ownable2Step {
+
+    constructor(address registry, address _owner) {
+        IOperatorFilterRegistry(registry).register(address(this));
+        transferOwnership(_owner);
+    }
+}
